e321fbf900db3ada7e149149cd83ed9bdbc8532d
Alex D'Agostino <alex@adrenaissance.com>
2025-07-13 15:49:05 +0200

    Add project files.

---
+++ RenaissanceRenegadeEngine/RenaissanceRenegadeEngine.cpp
@@ -0,0 +1,12 @@
+﻿// RenaissanceRenegadeEngine.cpp : Defines the entry point for the application.
+//
+
+#include "RenaissanceRenegadeEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
